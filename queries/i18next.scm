4396febbfd49daf0dc05a5fb3b5dd49e0ec18c65
nabekou29 <nabekou0729@gmail.com>
2024-08-20 05:04:42 +0900

    feat: add support for Trans and Translation components
    This commit adds support for the Trans and Translation components in the
    i18next query file. The changes include:
    
    - Adding a new rule for the Translation component with keyPrefix attribute.
    - Adding new rules for the Trans component with i18nKey and t attributes.

--- queries/i18next.scm
+++ queries/i18next.scm
@@ -37,7 +37,7 @@
     [
       (string (string_fragment) @i18n.namespace)
       (undefined)
-    ]
+    ]?
     (object
       (pair
         key: (property_identifier) @key_prefix_key (#eq? @key_prefix_key "keyPrefix")
@@ -45,6 +45,21 @@
       )?
     )?
   )
+) @i18n.get_t
+
+;; Translation コンポーネント
+(
+  jsx_opening_element
+    name: (identifier) @translation (#eq? @translation "Translation")
+    attribute: (jsx_attribute
+      (property_identifier) @key_prefix_attr (#eq? @key_prefix_attr "keyPrefix")
+      [
+        (string (string_fragment) @i18n.key_prefix) @i18n.key_prefix_arg
+        (jsx_expression
+          (string (string_fragment) @i18n.key_prefix) @i18n.key_prefix_arg
+        )
+      ]
+    )?
 ) @i18n.get_t
 
 ;; t 関数呼び出し
@@ -58,4 +73,41 @@
       (string_fragment) @i18n.key
     ) @i18n.key_arg
   )
+) @i18n.call_t
+
+
+;; Trans コンポーネント
+(
+  jsx_self_closing_element
+    name: (identifier) @trans (#eq? @trans "Trans")
+    attribute: (jsx_attribute
+      (property_identifier) @i18n_key (#eq? @i18n_key "i18nKey")
+      [
+       (string (string_fragment) @i18n.key) @i18n.key_arg
+       (jsx_expression
+         (string (string_fragment) @i18n.key) @i18n.key_arg
+       )
+      ]
+    )
+    attribute: (jsx_attribute
+      (property_identifier) @attr_t (#eq? @attr_t "t")
+      (_)
+    ) 
+) @i18n.call_t
+(
+  jsx_opening_element
+    name: (identifier) @trans (#eq? @trans "Trans")
+    attribute: (jsx_attribute
+      (property_identifier) @i18n_key (#eq? @i18n_key "i18nKey")
+      [
+       (string (string_fragment) @i18n.key) @i18n.key_arg
+       (jsx_expression
+         (string (string_fragment) @i18n.key) @i18n.key_arg
+       )
+      ]
+    )
+    attribute: (jsx_attribute
+      (property_identifier) @attr_t (#eq? @attr_t "t")
+      (_)
+    ) 
 ) @i18n.call_t
