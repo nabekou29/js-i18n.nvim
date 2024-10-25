a455cf7be44804ac5d2a3958b8cab3f062cab156
Naxdy <naxdy@naxdy.org>
2024-10-25 10:59:37 +0200

    feat: add support for namespace-based file structure

--- lua/js-i18n/lsp/checker.lua
+++ lua/js-i18n/lsp/checker.lua
@@ -33,10 +33,18 @@ function M.check(client, uri)
     local key = t_call.key
     local keys = vim.split(key, c.config.key_separator, { plain = true })
 
+    local namespace = nil
+
+    if c.config.namespace_separator ~= nil then
+      local split_first_key = vim.split(keys[1], c.config.namespace_separator, { plain = true })
+      namespace = split_first_key[1]
+      keys[1] = split_first_key[2]
+    end
+
     local missing_languages = {}
     local available_languages = t_source:get_available_languages()
     for _, lang in ipairs(available_languages) do
-      local translation = t_source:get_translation(lang, keys, library)
+      local translation = t_source:get_translation(lang, keys, library, namespace)
       if not translation then
         table.insert(missing_languages, lang)
       end
