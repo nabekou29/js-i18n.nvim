9cd1996ada3abcc200cc9c54ca8cd2ebbfe2a41f
nabekou29 <nabekou0729@gmail.com>
2026-02-14 19:10:28 +0900

    feat(server): add minimum server version check on init
    - Define MINIMUM_SERVER_VERSION = "0.4.0"
    - Check serverInfo.version from LSP initialize response
    - Warn user if server version is too old

--- lua/js-i18n/init.lua
+++ lua/js-i18n/init.lua
@@ -1,7 +1,10 @@
 local c = require("js-i18n.config")
+local utils = require("js-i18n.utils")
 local virt_text = require("js-i18n.virt_text")
 
 local M = {}
+
+local MINIMUM_SERVER_VERSION = "0.4.0"
 
 --- Get the js_i18n LSP client.
 --- @return vim.lsp.Client?
@@ -31,46 +34,57 @@ local function get_key_at_cursor(callback)
   local uri = vim.uri_from_bufnr(0)
   local cursor = vim.api.nvim_win_get_cursor(0)
   local position = { line = cursor[1] - 1, character = cursor[2] }
-  execute_command("i18n.getKeyAtPosition", { { uri = uri, position = position } }, function(err, result)
-    if err or not result or not result.key then
-      vim.schedule(function()
-        vim.notify("[js-i18n] No translation key found at cursor.", vim.log.levels.WARN)
-      end)
-      return
+  execute_command(
+    "i18n.getKeyAtPosition",
+    { { uri = uri, position = position } },
+    function(err, result)
+      if err or not result or not result.key then
+        vim.schedule(function()
+          vim.notify("[js-i18n] No translation key found at cursor.", vim.log.levels.WARN)
+        end)
+        return
+      end
+      callback(result.key)
     end
-    callback(result.key)
-  end)
+  )
 end
 
 --- Show vim.ui.input to edit a translation value and save it.
 --- @param lang string
 --- @param key string
 local function prompt_edit_translation(lang, key)
-  execute_command("i18n.getTranslationValue", { { lang = lang, key = key } }, function(val_err, val_result)
-    local current_value = ""
-    if not val_err and val_result and val_result.value then
-      current_value = val_result.value
-    end
-    vim.schedule(function()
-      vim.ui.input({
-        prompt = ("[%s] %s: "):format(lang, key),
-        default = current_value,
-      }, function(input)
-        if input == nil then
-          return
-        end
-        execute_command("i18n.editTranslation", {
-          { lang = lang, key = key, value = input },
-        }, function(edit_err)
-          if edit_err then
-            vim.schedule(function()
-              vim.notify("[js-i18n] Failed to edit translation: " .. tostring(edit_err), vim.log.levels.ERROR)
-            end)
+  execute_command(
+    "i18n.getTranslationValue",
+    { { lang = lang, key = key } },
+    function(val_err, val_result)
+      local current_value = ""
+      if not val_err and val_result and val_result.value then
+        current_value = val_result.value
+      end
+      vim.schedule(function()
+        vim.ui.input({
+          prompt = ("[%s] %s: "):format(lang, key),
+          default = current_value,
+        }, function(input)
+          if input == nil then
+            return
           end
+          execute_command("i18n.editTranslation", {
+            { lang = lang, key = key, value = input },
+          }, function(edit_err)
+            if edit_err then
+              vim.schedule(function()
+                vim.notify(
+                  "[js-i18n] Failed to edit translation: " .. tostring(edit_err),
+                  vim.log.levels.ERROR
+                )
+              end)
+            end
+          end)
         end)
       end)
-    end)
-  end)
+    end
+  )
 end
 
 --- Resolve lang, falling back to getCurrentLanguage from the server.
@@ -84,7 +98,10 @@ local function resolve_language(lang, callback)
   execute_command("i18n.getCurrentLanguage", {}, function(err, result)
     if err or not result or not result.language then
       vim.schedule(function()
-        vim.notify("[js-i18n] No current language set. Use :I18nSetLang first.", vim.log.levels.WARN)
+        vim.notify(
+          "[js-i18n] No current language set. Use :I18nSetLang first.",
+          vim.log.levels.WARN
+        )
       end)
       return
     end
@@ -132,6 +149,20 @@ M.setup = function(opts)
         virt_text.refresh_all()
       end,
     },
+    on_init = function(_, initialize_result)
+      local server_info = initialize_result and initialize_result.serverInfo
+      local server_version = server_info and server_info.version
+      if server_version and utils.compare_versions(server_version, MINIMUM_SERVER_VERSION) < 0 then
+        vim.notify_once(
+          ("[js-i18n] Server version %s is too old (minimum: %s).\nRun: npm install -g js-i18n-language-server"):format(
+            server_version,
+            MINIMUM_SERVER_VERSION
+          ),
+          vim.log.levels.WARN
+        )
+      end
+      return true
+    end,
   }
   vim.lsp.enable("js_i18n")
 
